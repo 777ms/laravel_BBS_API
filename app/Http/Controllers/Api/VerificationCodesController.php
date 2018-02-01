f432fb80e67b1688753d03aa83c7ff046d815e22
zsleader <zssen@vip.qq.com>
2018-02-01 08:36:16 +0000

    add register and using phone number

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -15,14 +15,21 @@ class VerificationCodesController extends Controller
         // 生成4位随机数，左侧补0
         $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
-        try {
-            $result = $easySms->send($phone, [
-                'content'  =>  "【zssen社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
-            ]);
-        } catch (\GuzzleHttp\Exception\ClientException $exception) {
-            $response = $exception->getResponse();
-            $result = json_decode($response->getBody()->getContents(), true);
-            return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
+        if(!app()->environment('production'))
+        {
+            $code = '1234';
+        }
+        else
+        {
+            try {
+                $result = $easySms->send($phone, [
+                    'content'  =>  "【zssen社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
+                ]);
+            } catch (\GuzzleHttp\Exception\ClientException $exception) {
+                $response = $exception->getResponse();
+                $result = json_decode($response->getBody()->getContents(), true);
+                return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
+            }
         }
 
         $key = 'verificationCode_'.str_random(15);
