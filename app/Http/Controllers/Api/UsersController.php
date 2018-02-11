d5a2c2e75e54141d252e198b5c1ad9d8fdc3fcda
zsleader <zssen@vip.qq.com>
2018-02-11 07:27:17 +0000

    add user update Api

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Transformers\UserTransformer;
 use App\Http\Requests\Api\UserRequest;
+use App\Models\Image;
 
 class UsersController extends Controller
 {
@@ -43,5 +44,20 @@ class UsersController extends Controller
     public function me()
     {
         return $this->response->item($this->user(), new UserTransformer());
+    }
+
+    public function update(UserRequest $request)
+    {
+        $user = $this->user();
+        $attribute = $request->all();
+
+        if($request->avatar_image_id)
+        {
+            $image = Image::find($request->avatar_image_id);
+            $attribute['avatar'] = $image->path;
+        }
+        $user->update($attribute);
+
+        return $this->response->item($user, new UserTransformer());
     }
 }
