6eabcdb30dfa576f15f150940399374eb88a61c9
zsleader <522376982@qq.com>
2018-02-24 06:33:58 +0000

    someone topic list

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,9 @@ $api->version('v1', [
         // 游客可以访问的接口
         $api->get('categories','categoriesController@index')->name('api.categories.index');
 
+        $api->get('topics','TopicsController@index')->name('api.topics.index');
 
+        $api->get('users/{user}/topics', 'TopicsController@userIndex')->name('api.users.topics.index');
         //需要token验证的接口
         $api->group(['middleware' => 'api.auth'],function($api){
             //当前登录用户信息
