6f9a84f072b61a379c63adbfd9ba844c843bdf92
zsleader <522376982@qq.com>
2018-02-24 06:46:12 +0000

    topic show

--- routes/api.php
+++ routes/api.php
@@ -53,6 +53,8 @@ $api->version('v1', [
 
         $api->get('topics','TopicsController@index')->name('api.topics.index');
 
+        $api->get('topics/{topic}','TopicsController@show')->name('api.topics.show');
+
         $api->get('users/{user}/topics', 'TopicsController@userIndex')->name('api.users.topics.index');
         //需要token验证的接口
         $api->group(['middleware' => 'api.auth'],function($api){
