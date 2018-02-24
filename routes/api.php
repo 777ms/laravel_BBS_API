44da53c747314c96d1bf8a15cd9f53a29c0f9692
zsleader <522376982@qq.com>
2018-02-24 02:53:26 +0000

    topic update api

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => 'serializer:array'
+    'middleware' => ['serializer:array','bindings']
 ], function($api) {
 
     $api->group([
@@ -62,7 +62,7 @@ $api->version('v1', [
             $api->patch('user','UsersController@update')->name('api.user.update');
             //发布话题
             $api->post('topics','TopicsController@store')->name('api.topics.store');
-
+            $api->patch('topics/{topic}', 'TopicsController@update')->name('api.topics.update');
         });
     });
 });
