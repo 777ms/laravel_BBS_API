54783b8b7cafa52b3eb2dec760cd4a35aef3e63b
zsleader <522376982@qq.com>
2018-02-24 08:20:07 +0000

    add replies store

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,8 @@ $api->version('v1', [
         $api->post('users', 'UsersController@store')
             ->name('api.users.store');
         //图片验证码
-        $api->post('captchas','CaptchasController@store')->name('api.captchas.store');
+        $api->post('captchas','CaptchasController@store')
+            ->name('api.captchas.store');
 
         //第三方登录
         $api->post('socials/{social_type}/authorizations','AuthorizationsController@socialStore')
@@ -49,25 +50,39 @@ $api->version('v1', [
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
         // 游客可以访问的接口
-        $api->get('categories','categoriesController@index')->name('api.categories.index');
+        $api->get('categories','categoriesController@index')
+            ->name('api.categories.index');
 
-        $api->get('topics','TopicsController@index')->name('api.topics.index');
+        $api->get('topics','TopicsController@index')
+            ->name('api.topics.index');
 
-        $api->get('topics/{topic}','TopicsController@show')->name('api.topics.show');
+        $api->get('topics/{topic}','TopicsController@show')
+            ->name('api.topics.show');
 
-        $api->get('users/{user}/topics', 'TopicsController@userIndex')->name('api.users.topics.index');
+        $api->get('users/{user}/topics', 'TopicsController@userIndex')
+            ->name('api.users.topics.index');
         //需要token验证的接口
         $api->group(['middleware' => 'api.auth'],function($api){
             //当前登录用户信息
-            $api->get('user','UsersController@me')->name('api.user.show');
+            $api->get('user','UsersController@me')
+                ->name('api.user.show');
             //图片资源
-            $api->post('images','ImagesController@store')->name('api.images.store');
+            $api->post('images','ImagesController@store')
+                ->name('api.images.store');
             //编辑登录用户信息
-            $api->patch('user','UsersController@update')->name('api.user.update');
+            $api->patch('user','UsersController@update')
+                ->name('api.user.update');
             //发布话题
-            $api->post('topics','TopicsController@store')->name('api.topics.store');
-            $api->patch('topics/{topic}', 'TopicsController@update')->name('api.topics.update');
-            $api->delete('topics/{topic}','TopicsController@destory')->name('api.topics.destory');
+            $api->post('topics','TopicsController@store')
+                ->name('api.topics.store');
+            $api->patch('topics/{topic}', 'TopicsController@update')
+                ->name('api.topics.update');
+            $api->delete('topics/{topic}','TopicsController@destory')
+                ->name('api.topics.destory');
+            //发布回复
+            $api->post('topics/{topic}/replies','RepliesController@store')
+                ->name('api.topics.replies.store');
+
         });
     });
 });
