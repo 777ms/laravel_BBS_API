71d9677d4d82c1b67bcd515e0fb57e8b0210fa5e
zsleader <522376982@qq.com>
2018-02-25 02:05:47 +0000

    add replies index

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Reply;
+use App\Models\User;
 use App\Http\Requests\Api\ReplyRequest;
 use App\Transformers\ReplyTransformer;
 
@@ -18,5 +19,30 @@ class RepliesController extends Controller
         $reply->save();
 
         return $this->response->item($reply, new ReplyTransformer())->setStatusCode(201);
+    }
+
+    public function destroy(Topic $topic, Reply $reply)
+    {
+        if ($reply->topic_id != $topic->id) {
+            return $this->response->errorBadRequest();
+        }
+
+        $this->authorize('destroy', $reply);
+        $reply->delete();
+
+        return $this->response->noContent();
+    }
+
+    public function index(Topic $topic)
+    {
+        $replies = $topic->replies()->paginate(20);
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+
+    public function userIndex(User $user)
+    {
+        $replies = $user->replies()->paginate(20);
+        return $this->response->paginator($replies, new ReplyTransformer());
     }
 }
