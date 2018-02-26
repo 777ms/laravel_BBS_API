7d97c045e1a262c090c581fc6b8d3ee72564230c
zsleader <522376982@qq.com>
2018-02-26 07:22:04 +0000

    add locale

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -73,7 +73,7 @@ class AuthorizationsController extends Controller
         $credentials['password'] = $request->password;
 
         if (!$token = \Auth::guard('api')->attempt($credentials)) {
-            return $this->response->errorUnauthorized('用户名或密码错误');
+            return $this->response->errorUnauthorized(trans('auth.failed'));
         }
 
         return $this->respondWithToken($token)->setStatusCode(201);
@@ -95,18 +95,6 @@ class AuthorizationsController extends Controller
     }
 
     public function destory()
-    {
-        Auth::guard('api')->logout();
-        return $this->response->noContent();
-    }
-
-    public function update()
-    {
-        $token = Auth::guard('api')->refresh();
-        return $this->respondWithToken($token);
-    }
-
-    public function destroy()
     {
         Auth::guard('api')->logout();
         return $this->response->noContent();
