e3124a2a01ea2308e6ebcfc401f4f954e2e706e3
zsleader <522376982@qq.com>
2018-02-26 03:11:03 +0000

    add links index

--- database/seeds/LinksTableSeeder.php
+++ database/seeds/LinksTableSeeder.php
@@ -8,7 +8,7 @@ class LinksTableSeeder extends Seeder
     public function run()
     {
         // 生成数据集合
-        $links = factory(Link::class)->times(6)->make();
+        $links = factory(Link::class)->times(8)->make();
 
         // 将数据集合转换为数组，并插入到数据库中
         Link::insert($links->toArray());
